3b3ced11ee19ae5124bdc00a139fd97936335d07
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-06-02 13:56:44 -0300

    Criando Controllers

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,61 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
 
-routes.get("/", (request, response) => {
-    return response.json({ message: "Hello World"})
+routes.get("/items", async (request, response) => {
+    const items = await knex('items').select('*');
+
+    const serializedItems = items.map(item => {
+        return { 
+            item: item.id,
+            name: item.title,
+            image_url : `http://localhost:3333/uploads/${item.image}`,
+        };
+    })
+    
+    return response.json(serializedItems);
+});
+
+routes.post("/points", async (request, response) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = request.body;
+
+    const trx = await knex.transaction();
+
+    const insertedIds = await trx('points').insert({
+        image: 'image-fake',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf
+    });
+
+    const point_id = insertedIds[0]
+
+    const pointItems = items.map((item_id: number) => {
+        return {
+            item_id,
+            point_id,
+        };
+    });
+
+    await trx('point_items').insert(pointItems);
+
+    return response.json( { success: true});
+
 });
 
 export default routes;
