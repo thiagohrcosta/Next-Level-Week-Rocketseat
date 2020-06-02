a9445bf2a6984ed15f505910a97f4ed7331de33c
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-06-02 11:15:49 -0300

    Iniciando Aula 02 - Criando rotas no backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,36 @@
-import express from 'express';
+import express, { request } from 'express';
 
 const app = express();
+
+const users = [
+    "Thiago",
+    "João",
+    "Maria",
+    "Ana"
+];
 
 app.get("/users", (request, response) => {
     console.log("Listagem de usuários");
 
-    response.json([
-        "Thiago",
-        "João",
-        "Maria"
-    ]);
+    response.json(users);
+});
+
+app.get("/users/:id", (request, response) => {
+
+    const id = Number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
+
 })
+
+app.post("/users", (request, response) => {
+    const user = {
+        name: "Thiago",
+        email: "1234@email.com.br"
+    }; 
+    return response.json(user);
+});
 
 app.listen(3333);
