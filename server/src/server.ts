cf3e6a313c799866d24f3a013076819d4c17070a
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-06-01 22:19:53 -0300

    Instalando o React

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,11 @@ const app = express();
 app.get("/users", (request, response) => {
     console.log("Listagem de usuários");
 
-    response.send("Hello World");
+    response.json([
+        "Thiago",
+        "João",
+        "Maria"
+    ]);
 })
 
 app.listen(3333);
