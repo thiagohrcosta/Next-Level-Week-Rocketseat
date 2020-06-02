f9370890c514fd45c44054c929330eb8eca646ba
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-06-01 22:24:59 -0300

    Finalizando Aula 01

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -7,18 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        
         <h1>HELLO WORLD!!!</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
