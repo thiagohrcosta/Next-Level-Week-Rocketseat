4944c8ebfb6a8a37db250e73b5145b968b6e1c1d
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-06-04 13:58:33 -0300

    Finalizando HTML da página principal

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import './App.css';
 
+import Home from './pages/Home';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <h1>HELLO WORLD!!!</h1>
-        
-      </header>
-    </div>
+   <Home />
   );
 }
 
