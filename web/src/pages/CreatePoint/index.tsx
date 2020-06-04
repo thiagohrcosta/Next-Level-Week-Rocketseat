730be4ba2e74ade5e0747d706f817956e683f020
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-06-04 15:28:41 -0300

    Iniciando parte Mapa da aplicaçaõ

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer,  Marker} from 'react-leaflet';
 
 import './styles.css';
 
