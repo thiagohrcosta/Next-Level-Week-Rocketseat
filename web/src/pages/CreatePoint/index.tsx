7c396808848de41a311b72da5b90d7dbae9a94db
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-06-12 15:52:12 -0300

    Finalizando lógica de busca por items da API

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer,  Marker} from 'react-leaflet';
+import api from '../../services/api';
 
 import './styles.css';
 
 import logo from '../../assets/logo.svg';
 
+interface Item {
+    id: number;
+    title: string;
+    image_url: string;
+}
+
 const CreatePoint = () => {
+
+    const [items, setItems] = useState<Item[]>([]);
+
+    useEffect(() => {
+        api.get('items').then(response => {
+            setItems(response.data);
+        });
+    }, []);
+    
     return (
         <div id="page-create-point">
             <header>
@@ -96,10 +112,13 @@ const CreatePoint = () => {
                     </legend>
 
                     <ul className="items-grid">
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt=""/>
-                            <span>Óleo de cozinha</span>
+                        {items.map(item => (
+                        <li key={item.id}>
+                            <img src={item.image_url} alt={item.title}/>
+                        <span>{item.title}</span>
                         </li>
+                        )
+                    )}
                     </ul>
                 </fieldset>
 
